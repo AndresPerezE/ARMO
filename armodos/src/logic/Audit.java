543f4534c2bea54c30943c10b3cedf59a54652e9
Estudiante <Estudiante@DESKTOP-V9DHAG5>
2016-03-30 12:52:35 -0500

    Audit Class added

---
+++ armodos/src/logic/Audit.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package logic;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Estudiante
+ */
+public class Audit {
+    private String idProcess;
+    private int operation;
+    private Date dateAudit;
+    private String username;
+    
+}
